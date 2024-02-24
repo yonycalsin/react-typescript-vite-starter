42e5e08084fa048a0f51326bd80446a8dc2efa6a
Yony Calsin <58490737+yonycalsin@users.noreply.github.com>
2024-02-24 13:38:26 -0500

    chore: purge dependencies

--- src/__tests__/basic.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-
-describe('basic', () => {
-  it('multiplication', () => {
-    expect(5 * 5).toMatchSnapshot()
-
-    expect(5 * 5).toStrictEqual(25)
-  })
-})
